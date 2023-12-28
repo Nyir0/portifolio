84fdff3a31ab876b0745898479ff123c76c22bbb
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2023-12-28 00:56:04 -0300

    vite update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
                 'resources/css/app.css', 
                 'resources/js/app.js',
                 'resources/js/canva/canva.js',
-                'resources/js/canva/vanta.globe.min.js'
+                'resources/js/canva/vanta.min.js'
             ],
             refresh: true,
         }),
